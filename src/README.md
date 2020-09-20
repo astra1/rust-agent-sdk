92781dc45d6df1555fbd6e4e869b1050d947a3a9
Maxim <astralfestival@gmail.com>
2020-09-20 18:40:21 +0300

    first compilable version

---
+++ src/README.md
@@ -0,0 +1,4 @@
+# rust_agent_sdk
+
+This crate provides a safe interface to [LiveEngage Agent SDK].
+
---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+extern crate hyper;
+
+mod client;
+
+mod connection;
+mod frame;
+
+pub use frame::Frame;
+pub use connection::Connection;
+
+/// Error returned by most functions.
+///
+/// check error handling crate
+///
+/// For performance reasons, boxing is avoided in any hot path. For example, in
+/// `parse`, a custom error `enum` is defined. This is because the error is hit
+/// and handled during normal execution when a partial frame is received on a
+/// socket. `std::error::Error` is implemented for `parse::Error` which allows
+/// it to be converted to `Box<dyn std::error::Error>`.
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+/// A specialized `Result` type for mini-redis operations.
+///
+/// This is defined as a convenience.
+pub type Result<T> = std::result::Result<T, Error>;
